baf4969d9c625797f9f7202e47ff26bbf4e4b72f
Joe Cheng <joe@rstudio.com>
2019-12-19 22:17:14 -0800

    Fake it

--- make-node.sh
+++ make-node.sh
@@ -15,4 +15,5 @@ cd ${NODE_DIR}
 export CPPFLAGS=-D__STDC_FORMAT_MACROS
 export LDFLAGS=-lrt
 scl enable devtoolset-7 python27 ./configure
-scl enable devtoolset-7 python27 "ARCH=x64 make -j$(nproc) binary"
+#scl enable devtoolset-7 python27 "ARCH=x64 make -j$(nproc) binary"
+touch node-v12.14.0-linux-x64.tar.xz
